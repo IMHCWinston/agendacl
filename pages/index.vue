878107a37f8da985499083604b11202eb044d9d0
Winston Wijaya <winstonwijaya88@gmail.com>
2021-06-10 13:31:09 +0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -507,6 +507,7 @@
 <script>
 export default {
   async asyncData({ $axios }) { //asyncData hook is called before created hook, use to preload components
+    alert(process.env.BASE_URL);
     const url = await $axios.$get('api/sign-in-url');
     return { url } //url is appended to vue data property
   }
